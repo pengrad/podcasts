4502b434e86362faf46f8d1ee5d9fd73d3f223f4
Stas Parshin <s.a.parshin@gmail.com>
2015-08-27 00:41:57 +0300

    basic listview with search results

--- src/main/java/com/github/pengrad/podcasts/MainActivity.java
+++ src/main/java/com/github/pengrad/podcasts/MainActivity.java
@@ -6,21 +6,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener, MenuItemCompat.OnActionExpandListener {
 
     private SearchView mSearchView;
+    @Bind(R.id.listview) ListView mListview;
+    private ArrayAdapter<ItunesResult.Podcast> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<>());
+        mListview.setAdapter(mAdapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItemCompat.setOnActionExpandListener(menu.findItem(R.id.menu_search), this);
         mSearchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        mSearchView.setOnQueryTextListener(this);
         return true;
     }
 
@@ -33,5 +49,34 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        new ItunesSearchAPI()
+                .searchPodcasts(getApplicationContext(), query)
+                .setCallback((e, result) -> {
+                    mAdapter.clear();
+                    if (result != null) {
+                        mAdapter.addAll(result.results);
+                    }
+                });
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean onMenuItemActionExpand(MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public boolean onMenuItemActionCollapse(MenuItem item) {
+        mAdapter.clear();
+        return false;
     }
 }
